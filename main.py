f43a0e00984bd8e3f251f6caabf2d231103ab9b9
Guillem Borrell <guillemborrell@gmail.com>
2014-07-14 12:22:24 +0200

    Models to a separate file

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import webapp2
 import jinja2
 from google.appengine.api import channel, users
 from google.appengine.ext import ndb
+from models import *
 
 
 JINJA_ENVIRONMENT = jinja2.Environment(
@@ -15,50 +16,6 @@ JINJA_ENVIRONMENT = jinja2.Environment(
                      'templates')),
     extensions = ['jinja2.ext.autoescape'],
     autoescape = True)
-
-
-class ChatManager(ndb.Model):
-    name = ndb.StringProperty()
-    date = ndb.DateTimeProperty(auto_now_add=True)
-    clients = ndb.StringProperty(repeated=True)
-    active = ndb.BooleanProperty()
-    owner = ndb.UserProperty()
-    options = ndb.JsonProperty()
-
-    @classmethod
-    def query_last(cls,num):
-        return cls.query(cls.active == True).order(-cls.date).fetch(num)
-
-    def num_clients(self):
-        return len(self.clients)
-
-    def add_client(self, client):
-        client_list = self.clients
-        client_list.append(client)
-        self.put()
-
-    def remove_client(self, client):
-        client_list = self.clients
-        while client in client_list: client_list.remove(client)
-        self.put()
-
-
-class Message(ndb.Model):
-    date = ndb.DateTimeProperty(auto_now_add=True)
-    author = ndb.StringProperty()
-    client = ndb.StringProperty()
-    text = ndb.StringProperty()
-
-    @classmethod
-    def query_last_from_chat(cls,num,chat_key):
-        query = cls.query(ancestor=ndb.Key(urlsafe=chat_key))
-        return query.order(-cls.date).fetch(10)
-
-
-class Event(ndb.Model):
-    date = ndb.DateTimeProperty(auto_now_add=True)
-    kind = ndb.StringProperty()
-    data = ndb.JsonProperty()
     
 
 class ServeToken(webapp2.RequestHandler):
